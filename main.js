b3650d2e801202a006102b62a88b7655694276a2
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-01 20:58:25 -0400

    clock working, hov progress

--- main.js
+++ main.js
@@ -2,17 +2,30 @@ const $clock = document.querySelector('clock');
 const $background = document.querySelector('.clock-face')
 const $currentDisplay = document.querySelector('.clock-display');
 console.log($currentDisplay);
+const $progressBar = document.querySelector('.clock-progress-bar');
+
 
 
 // showTime.addEventListener("mouseover", hexCode);
 
 
 function showTime () {
+    $currentDisplay.addEventListener("mouseenter", mouseEnter);
+    $currentDisplay.addEventListener("mouseleave", mouseLeave);
+
+    function mouseEnter() {
+    $currentDisplay.textContent = color;
+}
+
+    function mouseLeave() {
+    $currentDisplay.textContent = currentTime;
+}
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
     let session = "AM";
+    var color;
 
     if (hour > 12) {
         hour = hour - 12;
@@ -28,17 +41,18 @@ function showTime () {
     min = (min < 10) ? "0" + min : min;
     sec = (sec < 10) ? "0" + sec : sec;
 
-    let currentTime = hour + ":" + min + ":" + sec;
+    var currentTime = hour + ":" + min + ":" + sec;
     $currentDisplay.textContent = currentTime;
 
-    console.log(currentTime);
-    // color = '#' + hour + min + sec;
-    // document.body.style.background = color;
+    color = '#' + hour + min + sec;
+    console.log(currentTime, color);
+    $background.style.background = color;
     // document.getElementsByClassName("clock").innerHTML = color;
 
+    $progressBar.style.width = (( min / 60 ) * 100) + "%";
+    console.log((( min / 60 ) * 100) + "%");
 
-    // hexCode = "#" + hour + min + sec;
-    // document.getElementsByClassName("clock-display").innerHTML = hexCode;
+    
 
     // const counter = document.querySelector('clock-display'); 
 }
